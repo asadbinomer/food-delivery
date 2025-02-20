5416dc6c72700279cb860493586782ed9b317c33
Ubaidullah Omer <ranaasadullah885@gmail.com>
2025-02-20 11:40:46 +0500

    GFH

---
+++ food-delivery/src/component/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FoodDisplay = () => {
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default FoodDisplay
--- food-delivery/src/context/StoreContext.jsx
+++ food-delivery/src/context/StoreContext.jsx
@@ -1,10 +1,11 @@
 import { createContext, } from "react";
+import { food_list, } from "../assets/frontend_assets/assets";
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
    const contextValue = {
-      
+      food_list
    }
 
    return (
